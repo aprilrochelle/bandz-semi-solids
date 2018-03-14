443e986cbc1fcb00554cbb99084923323b2672d1
Amanda Slayton <alslayton86@gmail.com>
2018-03-13 20:36:33 -0500

    wip

---
+++ javascript/main.js
@@ -0,0 +1,142 @@
+var Songs = [
+  {
+    songId: 1,
+    album: "Stick in the Mud",
+    name: "Stuck On You",
+    duration: 186,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 2,
+    album: "Stick in the Mud",
+    name: "My Ooze",
+    duration: 203,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 3,
+    album: "Stick in the Mud",
+    name: "Laughin' In The Dark",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 4,
+    album: "Stick in the Mud",
+    name: "Play with my Heart",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 5,
+    album: "Stick in the Mud",
+    name: "Brush It Up",
+    duration: 146,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 6,
+    album: "Stick in the Mud",
+    name: "It Ain't Natural",
+    duration: 359,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 7,
+    album: "Stick in the Mud",
+    name: "Smash the Spud",
+    duration: 465,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 8,
+    album: "Stick in the Mud",
+    name: "Slippin'",
+    duration: 157,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 9,
+    album: "Stick in the Mud",
+    name: "Can't Eat This (Pain I'm Feelin')",
+    duration: 791,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 10,
+    album: "Stick in the Mud",
+    name: "Stick It (And See What Happens)",
+    duration: 465,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 11,
+    album: "Single",
+    name: "I'm Just an Ooze",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 12,
+    album: "Single",
+    name: "Let it rise",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 13,
+    album: "Single",
+    name: "Melt My Gouda",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 14,
+    album: "Single",
+    name: "Minty Fresh",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 15,
+    album: "Single",
+    name: "Squish Me, I'm Gooey",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 16,
+    album: "Single",
+    name: "Slem",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 17,
+    album: "Single",
+    name: "My Friend, The Gelatinous Cube",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 18,
+    album: "Single",
+    name: "Jello, Jello, Jello",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 19,
+    album: "Single",
+    name: "Slick It",
+    duration: 105,
+    audioURL: "audioclips/"
+  },
+  {
+    songId: 20,
+    album: "Single",
+    name: "Playtime",
+    duration: 105,
+    audioURL: "audioclips/"
+  }
+]
